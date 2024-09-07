8aa6a117821481f0d73c8e78e147a559df1a9d51
davidho27941 <davidho@gapp.nthu.edu.tw>
2024-09-07 16:17:57 +0800

    chore: rename reference table.'

--- weather_data_dbt/models/artifacts/stations/existing_stations.sql
+++ weather_data_dbt/models/artifacts/stations/existing_stations.sql
@@ -1,5 +1,5 @@
 with all_stations_information AS (
-    select * from {{ ref('int_stations_joined_all_infomation') }}
+    select * from {{ ref('int_stations_join_all_infomation') }}
 )
 
 select * from all_stations_information WHERE station_status = '現存測站'
